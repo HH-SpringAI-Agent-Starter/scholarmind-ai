910b43fa95e2a140c6e299e69b7b3c015aa36e7e
javaAgentAI <3159979021@qq.com>
2026-07-19 20:21:59 +0800

    feat: add scholarmind AI academic research assistant [skip ci]

---
+++ scholarmind-ai/src/main/java/com/springai/scholarmind/ScholarMindApplication.java
@@ -0,0 +1,12 @@
+package com.springai.scholarmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarMindApplication.class, args);
+    }
+}
